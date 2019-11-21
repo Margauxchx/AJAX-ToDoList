3f2057f3ef7019c29ec8e6b80be90761d1956e00
margauxchx <margaux.chaix@yahoo.fr>
2019-11-21 15:53:43 +0100

    Push

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -6,7 +6,11 @@ class EmailsController < ApplicationController
   def create
     @emails = Email.all
     @emails = Email.create(object: Faker::Book.title, body: "Coucou je suis le content de l'email")
-   redirect_to emails_path
+    
+    respond_to do |format|
+        format.html { redirect_to emails_path }
+        format.js { }
+    end
   end
   
   def show
